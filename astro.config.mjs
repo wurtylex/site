1697ba9fe0f83e5c380ddab6db6ce92d243a0711
wurtylex <anthony.c.chang10@gmail.com>
2026-03-19 15:55:29 -0400

    added math

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import react from '@astrojs/react';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://wurtylex.github.io',
@@ -9,5 +12,9 @@ export default defineConfig({
     mdx(),
     react(),
   ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
   output: 'static',
 });
