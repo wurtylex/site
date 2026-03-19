214fb1fca800a06edca9380250b041cc22dab10f
wurtylex <anthony.c.chang10@gmail.com>
2026-03-19 14:58:49 -0400

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), react()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
